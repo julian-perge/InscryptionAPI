ee3f34627c3086fd3ae8493e58deefe80b1c9785
julian-perge <julianperge@gmail.com>
2022-01-03 13:00:53 -0500

    Added Prefix method for changing the original logic of CardDisplayer3D.UpdateTribeIcon to allow for custom tribes.

--- Patches/CardDisplayer3D.cs
+++ Patches/CardDisplayer3D.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using APIPlugin;
 using DiskCardGame;
 using HarmonyLib;
@@ -5,10 +6,11 @@ using UnityEngine;
 
 namespace API.Patches
 {
-	[HarmonyPatch(typeof(CardDisplayer3D), "GetEmissivePortrait")]
-	public class CardDisplayer3D_GetEmissivePortrait
+	[HarmonyPatch(typeof(CardDisplayer3D))]
+	public class CardDisplayer3DPatches
 	{
-		static bool Prefix(Sprite mainPortrait, ref Sprite __result)
+		[HarmonyPrefix, HarmonyPatch(nameof(CardDisplayer3D.GetEmissivePortrait))]
+		private static bool GetEmissiveSpritePrefix(Sprite mainPortrait, ref Sprite __result)
 		{
 			Sprite sprite;
 			if (NewCard.emissions.TryGetValue(mainPortrait.name, out sprite))
@@ -16,12 +18,42 @@ namespace API.Patches
 				__result = sprite;
 				return false;
 			}
+
 			if (CustomCard.emissions.TryGetValue(mainPortrait.name, out sprite))
 			{
 				__result = sprite;
 				return false;
 			}
+
 			return true;
+		}
+
+
+		[HarmonyPrefix, HarmonyPatch(nameof(CardDisplayer3D.UpdateTribeIcon))]
+		public static bool SetTribeIconPrefix(CardInfo info, CardDisplayer3D __instance)
+		{
+			var cardOfTribe = NewTribe.Tribes
+				.Where(tribeWithSprite => info.IsOfTribe(tribeWithSprite.Key));
+
+			// there are 5 TribeIconRenderers
+			// Amalgam is the only card that uses all 5 renders
+			foreach (var iconRenderer in __instance.tribeIconRenderers)
+			{
+				iconRenderer.sprite = null;
+			}
+
+			foreach (var tribe in cardOfTribe)
+			{
+				Plugin.Log.LogDebug($"Card [{info.name}] Tribe is [{tribe.Key}]");
+				foreach (var spriteRenderer in __instance.tribeIconRenderers.Where(renderer => renderer.sprite == null))
+				{
+					// Plugin.Log.LogDebug($"-> Setting spriteRenderer to tribe");
+					spriteRenderer.sprite = tribe.Value;
+					break;
+				}
+			}
+
+			return false;
 		}
 	}
 }
